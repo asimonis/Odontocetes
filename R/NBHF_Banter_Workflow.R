fee7920e86930238cf5ba015d6420c3a615b6cc3
asimonis <anne.simonis@gmail.com>
2024-06-03 14:51:24 -0700

    Update banter workflow

--- R/NBHF_Banter_Workflow.R
+++ R/NBHF_Banter_Workflow.R
@@ -19,8 +19,23 @@ freshRun = FALSE
 
 if(freshRun==TRUE){
 #Generate Acoustic Study
-dbFolder<-'H:/Odontocetes/NBHF/Labeled Events/Databases'
-binFolder<-'H:/Odontocetes/NBHF/Binaries'
+# dbFolder<-'H:/Odontocetes/NBHF/Labeled Events/Databases'
+# binFolder<-'H:/Odontocetes/NBHF/Binaries'
+  dbFolder<-'/Volumes/ADRIFT_Analysis/Odontocetes/NBHF/Labeled Events/Databases/'
+  binFolder<-'/Volumes/ADRIFT_Analysis/Odontocetes/NBHF/Binaries/'
+  
+#Add GPS to databases
+gpsFolder<-'/Users/ASimonis/Documents/ADRIFT Analysis/GPS_CSV'
+dbFiles<-list.files(path=dbFolder,pattern='.sqlite3',full.names = TRUE)
+gpsFiles<-list.files(path=gpsFolder,pattern='.csv',full.names=TRUE)
+
+dbDep<- str_extract(dbFiles,'ADRIFT_\\d{3}')
+gpsDep<-str_extract(gpsFiles,'ADRIFT_\\d{3}')
+
+for(d in 1:nrow(dbFiles)){
+gpsInd<-which(gpsDep==dbDep[d])
+  addPgGps(dbFiles[d],gpsFiles[gpsInd],source='csv')
+}
 
 pps <- PAMpalSettings(dbFolder, binFolder, sr_hz='auto', filterfrom_khz=100, filterto_khz=160, winLen_sec=.0025)
 ADRIFT_NBHF <- processPgDetections(pps, mode='db')
